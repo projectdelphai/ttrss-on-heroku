3bb957f91cdb3d4435a1a192f943c3edc8ca4bbf
Alexandre Joseph <jexhson@alexandrejoseph.com>
2013-06-13 21:02:45 +0200

    Update ttrss to v1.8

--- ttrss_heroku_setup.sh
+++ ttrss_heroku_setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 clear
-echo -n "The ttrss code will be placed in a folder under $PWD. Is this okay? Y/N: " 
+echo -n "The ttrss code will be placed in a folder under $PWD. Is this okay? Y/N: "
 read query
 if [ "$query" != Y ]; then
   exit 0
@@ -19,13 +19,13 @@ git init
 heroku create $appname
 echo "Registering application online. . ."
 sleep 20
-wget https://github.com/gothfox/Tiny-Tiny-RSS/archive/1.7.9.tar.gz -O "1.7.9.tar.gz"
-tar -xvzf "1.7.9.tar.gz"
-cd Tiny-Tiny-RSS-1.7.9
+wget https://github.com/gothfox/Tiny-Tiny-RSS/archive/1.8.tar.gz -O "1.8.tar.gz"
+tar -xvzf "1.8.tar.gz"
+cd Tiny-Tiny-RSS-1.8
 mv * ../
 cd ..
-rm "1.7.9.tar.gz"
-rm -r Tiny-Tiny-RSS-1.7.9
+rm "1.8.tar.gz"
+rm -r Tiny-Tiny-RSS-1.8
 echo -e "\n"
 echo -n "Finished with the source code files. Right now there's no proper configuration or database to store our feeds. Let's fix that, shall we? Y/N: "
 read query
